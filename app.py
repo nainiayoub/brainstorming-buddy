0876edeae28ffe94014cf31c44280848c9d70d7e
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-11-22 22:04:51 +0100

    about info

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ with st.sidebar:
     """)
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
     st.markdown("""
-    You can help sustain the project.<br/>
+    You can help support the project<br/>
     Find me at: [Twitter](https://twitter.com/nainia_ayoub) | [LinkedIn](https://www.linkedin.com/in/ayoub-nainia/?locale=en_US) | [GitHub](https://github.com/nainiayoub) 
     
     """,
